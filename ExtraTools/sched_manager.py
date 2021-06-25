ec8433847a3add893fe9b36800ab148853384f93
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-06-25 18:20:58 -0300

    New feature

--- ExtraTools/sched_manager.py
+++ ExtraTools/sched_manager.py
@@ -7,8 +7,23 @@ def sched():
     pass
 
 
-@sched.command(help='Run a task')
-def run():
-    sc.every(2).seconds.do(system, command=r'python C:\Users\Kevin\Documents\GitHub\Lash\__init__.py random ')
-    while True:
-        sc.run_pending()
+@sched.command()
+@click.argument('command', metavar='command', type=click.STRING)
+@click.option('-s', type=click.INT, help='Set delay seconds')
+@click.option('-m', type=click.INT, help='Set delay minutes')
+@click.option('-h', type=click.INT, help='Set delay hours')
+def run(command, s, m, h):
+    """Run commands repetitively at a given interval starting from
+       current moment.
+
+       Example: lash sched run -s 10 "help" """
+    if not s and not m and not h:
+        raise Exception('Error, time delay is not defined')
+    if s and m and h or s and m or s and h or h and m:
+        raise Exception('Error, not support for multi time options')
+    else:
+        sc.every(s).seconds.do(system, command=command) if s else None
+        sc.every(m).minutes.do(system, command=command) if m else None
+        sc.every(h).hours.do(system, command=command) if h else None
+        while True:
+            sc.run_pending()

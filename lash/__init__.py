64badda23437d1c06d078cf06d12b4f279e5dc07
KevBoyz <Kevinho_gameplays@hotmail.com>
2021-10-23 19:24:17 -0300

    Version name fix

--- lash/__init__.py
+++ lash/__init__.py
@@ -8,7 +8,7 @@ from lash.ExtraTools import *
 @group('global')
 def Global():
     """\b
-        - Lash 1.1.3 by KevBoyz ~ https://github.com/KevBoyz/Lash
+        - Lash 1.1.3.1 by KevBoyz ~ https://github.com/KevBoyz/Lash
     """
     ...
 

fcd9a4431bda4f63cb42719d35bad46ac1e48a53
KevBoyz <Kevinho_gameplays@hotmail.com>
2021-12-05 15:07:34 -0300

    update

--- lash/Exportables/imageTools.py
+++ lash/Exportables/imageTools.py
@@ -1,17 +1,35 @@
 from PIL import Image
+from lash.Exportables.fileTools import *
 
 
-def flipp(im, file, lr=False, tb=False):
+def save(im, file):
+    try:
+        im.save(file)
+    except ValueError as e:  # unknown file extension
+        print(f'Error: {e}')
+
+
+def f_flip(im, file, lr=False, tb=False):
     if lr:
         im_flipped = im.transpose(Image.FLIP_LEFT_RIGHT)
-        try:
-            im_flipped.save(file)
-        except ValueError as e:  # unknown file extension
-            print(f'Error: {e}')
+        save(im_flipped, file)
     elif tb:
         im_flipped = im.transpose(Image.FLIP_TOP_BOTTOM)
-        try:
-            im_flipped.save(file)
-        except ValueError as e:  # unknown file extension
-            print(f'Error: {e}')
+        save(im_flipped, file)
 
+
+def re_size(im, file, axis, d, r):
+    if not axis and not d and not r:
+        print('Error, none action received, send some option, --help for more details')
+        return
+    else:
+        if axis:
+            x, y = axis
+            im_rszd = im.resize((x, y))
+            save(im_rszd, file)
+        elif d:
+            im_rszd = im.resize((int(im.size[0] * 2), int(im.size[1] * 2)))
+            save(im_rszd, file)
+        elif r:
+            im_rszd = im.resize((int(im.size[0] / 2), int(im.size[1] / 2)))
+            save(im_rszd, file)
--- lash/image_handler.py
+++ lash/image_handler.py
@@ -9,7 +9,7 @@ def image():
     ...
 
 
-@image.command(help='Flip a image')
+@image.command(help='Flip image(s)')
 @click.argument('path', metavar='<path>', type=click.Path(exists=True))
 @click.option('-all', type=click.STRING, help='Edit all images on path with x extension Ex: -all ".png" ')
 @click.option('-lr', is_flag=True, help='Mirror left to right')
@@ -18,25 +18,50 @@ def flip(path, all, lr, tb):
     if not lr and not tb:
         print('Error, none action received, send some option, --help for more details')
     if all:
-        _type = all  # Semantic var
-        n_editions = 0  # Files modded
+        _type = all
+        n_editions = 0
         os.chdir(path)
         for root, folders, files in os.walk('.'):
             for file in files:
                 if file.endswith(_type):
                     if lr:
-                        flipp(Image.open(os.path.join(root, file)), file, lr=True)
+                        f_flip(Image.open(os.path.join(root, file)), file, lr=True)
                     elif tb:
-                        flipp(Image.open(os.path.join(root, file)), file, tb=True)
+                       f_flip(Image.open(os.path.join(root, file)), file, tb=True)
                     n_editions += 1
         print(f'Process Completed, {n_editions} files edited')
     else:
         file = get_file(path)
         im = Image.open(file)
         if lr:
-            flipp(im, file, lr=True)
+            f_flip(im, file, lr=True)
         elif tb:
-            flipp(im, file, tb=True)
+            f_flip(im, file, tb=True)
+        print('Process completed')
+
+
+@image.command(help='Resize image(s)')
+@click.argument('path', metavar='<path>', type=click.Path(exists=True))
+@click.option('-axis', nargs=2, type=click.INT, help='Set new values for x, y as you want: -axis 400 300')
+@click.option('-d', is_flag=True, help='Double image size')
+@click.option('-r', is_flag=True, help='Reduce image size (size / 2)')
+@click.option('-all', type=click.STRING, help='Edit all images on path with x extension Ex: -all ".png" ')
+def resize(path, axis, d, r, all):
+    if all:
+        _type = all
+        n_editions = 0
+        os.chdir(path)
+        for root, folders, files in os.walk('.'):
+            for file in files:
+                if file.endswith(_type):
+                    re_size(Image.open(os.path.join(root, file)), file, axis, d, r)
+                    n_editions += 1
+        print(f'Process completed, {n_editions} files edited')
+    else:
+        file = get_file(path)
+        im = Image.open(file)
+        re_size(im, file, axis, d, r)
+        print('Process completed')
 
 
 

e07234769dbafecc1cb0899e9f34414a40dc8a53
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-22 17:26:05 -0300

    video make command

---
+++ lash/Exportables/videoTools.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+from PIL import Image
+from tqdm import tqdm
+
+
+def resize_images():
+    path = os.path.join(os.getcwd())
+    mean_height = 0
+    mean_width = 0
+    num_of_images = 0
+    for file in os.listdir('.'):
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
+            num_of_images += 1
+            im = Image.open(os.path.join(path, file))
+            width, height = im.size
+            mean_width += width
+            mean_height += height
+    mean_width = int(mean_width / num_of_images)
+    mean_height = int(mean_height / num_of_images)
+    pbar = tqdm(total=len(os.listdir('.') * 2), colour='green')
+    pbar.set_description('Processing images')
+    for file in os.listdir('.'):
+        pbar.update(1)
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
+            im = Image.open(os.path.join(path, file))
+            width, height = im.size
+            imResize = im.resize((mean_width, mean_height), Image.ANTIALIAS)
+            imResize.save(file, 'JPEG', quality=95)
+    return pbar
--- lash/video_handler.py
+++ lash/video_handler.py
@@ -1,6 +1,45 @@
 import click
+import os
+import cv2
+from PIL import Image
+from tqdm import tqdm
+from lash.Exportables.videoTools import *
 
 
 @click.group('video', help='Video tolls')
 def video():
     ...
+
+
+@video.command(help='Build a video with multiple images')
+@click.argument('path', metavar='<path>', type=click.Path(exists=True), required=False, default='.')
+@click.option('-fps', type=click.INT, help='Fps', default=5, show_default=True)
+@click.option('-n', type=click.STRING, help='Video name', default='video', show_default=True)
+def make(path, fps, n):
+    os.chdir(path)
+    pbar = resize_images()
+    image_folder = os.getcwd()
+    video_name = f'{n}.avi'
+    os.chdir('..')
+    images_list = [img for img in os.listdir(image_folder)
+                   if img.endswith(".jpg") or
+                   img.endswith(".jpeg") or
+                   img.endswith("png")]
+    intnumbs = list()
+    for file in images_list:
+        intnumbs.append(file[:file.find('.')])
+        intnumbs.sort(key=int)
+    images = list()
+    for i in intnumbs:
+        images.append(i + '.png')
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter(video_name, 0, fps, (width, height))
+    for image in images:
+        pbar.update(1)
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+    cv2.destroyAllWindows()
+    video.release()
+    pbar.close()
+    print(f'Video built and saved in {os.path.join(os.getcwd(), video_name)}')
+

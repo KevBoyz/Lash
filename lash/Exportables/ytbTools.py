8dd20ae7c3a67bf2c1bf81c476643f3b03609797
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-22 11:29:58 -0300

    code upgraded

---
+++ lash/Exportables/ytbTools.py
@@ -0,0 +1,46 @@
+from math import ceil
+from pytube import YouTube, Search
+from timeit import default_timer
+
+
+def get_video_by_link(yt, low):
+    tic = default_timer()
+    if low:
+        video = yt.streams.get_lowest_resolution()
+    else:
+        video = yt.streams.get_highest_resolution()
+    toc = default_timer()
+    print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | Downloading: {video.title}')
+    totalsz = (video.filesize / 1024) / 1024
+    return video, totalsz
+
+
+def get_video_by_search(s, low):
+    tic = default_timer()
+    search = Search(s)
+    if low:
+        video = search.results[0].streams.get_lowest_resolution()
+    else:
+        video = search.results[0].streams.get_highest_resolution()
+    toc = default_timer()
+    print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | Downloading: {video.title}')
+    return video
+
+
+def get_audio_by_link(yt):
+    tic = default_timer()
+    video = yt.streams.filter(only_audio=True).first()
+    toc = default_timer()
+    print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | (Audio only) Downloading: {video.title}')
+    totalsz = int((video.filesize / 1024) / 1024)
+    return video, totalsz
+
+
+def get_audio_by_search(s):
+    tic = default_timer()
+    search = Search(s)
+    video = search.results[0].streams.filter(only_audio=True).first()
+    toc = default_timer()
+    print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | (Audio only) Downloading: {video.title}')
+    return video
+
--- lash/ExtraTools/youtube.py
+++ lash/ExtraTools/youtube.py
@@ -5,9 +5,10 @@ import pathlib
 from timeit import default_timer
 from math import ceil
 from tqdm import tqdm
+from lash.Exportables.ytbTools import *
 
 
-downloads_file = os.path.join(pathlib.Path.home(), 'Downloads')
+downloads_folder = os.path.join(pathlib.Path.home(), 'Downloads')
 
 
 @click.group('yt', help='Youtube tools')
@@ -16,16 +17,13 @@ def yt():
 
 
 def on_progress(vid, chunk, bytes_remaining):
-    total_size = vid.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    percentage_of_completion = bytes_downloaded / total_size * 100
-    totalsz = (total_size / 1024) / 1024
-    totalsz = round(totalsz, 1)
-    remain = (bytes_remaining / 1024) / 1024
-    remain = round(remain, 1)
-    dwnd = (bytes_downloaded / 1024) / 1024
-    dwnd = round(dwnd, 1)
-    percentage_of_completion = round(percentage_of_completion, 2)
+    # bytes_downloaded = total_size - bytes_remaining
+    # percentage_of_completion = bytes_downloaded / total_size * 100
+    # dwnd = (bytes_downloaded / 1024) / 1024
+    # dwnd = round(dwnd, 1)
+    # percentage_of_completion = round(percentage_of_completion, 2)
+    totalsz = round((vid.filesize / 1024) / 1024, 1)
+    remain = round((bytes_remaining / 1024) / 1024, 1)
     p_bar.reset()
     p_bar.update(int(totalsz - remain))
     p_bar.refresh()
@@ -35,47 +33,28 @@ def on_progress(vid, chunk, bytes_remaining):
 @click.option('-l', type=click.STRING, help='video link')
 @click.option('-s', type=click.STRING, help='catch video searching')
 @click.option('-a', is_flag=True, help='audio only')
-@click.option('-f', type=click.Path(), default=downloads_file, show_default=True, help='output folder')
+@click.option('-f', type=click.Path(), default=downloads_folder, show_default=True, help='output folder')
 @click.option('-low', is_flag=True, help='low resolution')
 def download(l, s, a, f, low):
-    if not s:
-        yt = YouTube(l, on_progress_callback=on_progress)
     global p_bar
     print('Getting video', end='')
-    if a:
-        tic = default_timer()
-        video = yt.streams.filter(only_audio=True).first()
-        toc = default_timer()
-        print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | (Audio only) Downloading: {video.title}')
-        totalsz = int((video.filesize / 1024) / 1024)
+    if l and not a:
+        yt = YouTube(l, on_progress_callback=on_progress)
+        video, totalsz = get_video_by_link(yt, low)
         p_bar = tqdm(range(int(totalsz)), colour='green')
+        video.download(f)
+    elif a:
+        if l:
+            yt = YouTube(l, on_progress_callback=on_progress)
+            video, totalsz = get_audio_by_link(yt)
+            p_bar = tqdm(range(int(totalsz)), colour='green')
+        elif s:
+            video = get_audio_by_search(s)
         out_file = video.download(output_path=f)
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         os.rename(out_file, new_file)
-    elif s:
-        tic = default_timer()
-        search = Search(s)
-        video = search.results[0].streams.get_lowest_resolution()
-        toc = default_timer()
-        print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | Downloading: {video.title}')
-        totalsz = int((video.filesize / 1024) / 1024)
+    elif s and not l:
+        video = get_video_by_search(s, low)
         video.download(f)
-    else:
-        if low:
-            tic = default_timer()
-            video = yt.streams.get_lowest_resolution()
-            toc = default_timer()
-            print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | Downloading: {video.title}')
-            totalsz = int((video.filesize / 1024) / 1024)
-            p_bar = tqdm(range(int(totalsz)), colour='green')
-            video.download(f)
-        else:
-            tic = default_timer()
-            video = yt.streams.get_highest_resolution()
-            toc = default_timer()
-            print(f' - Time Elapsed: {ceil((toc - tic) / 60)}min | Downloading: {video.title}')
-            total_size = video.filesize
-            totalsz = (total_size / 1024) / 1024
-            p_bar = tqdm(range(int(totalsz)), colour='green')
-            video.download(f)
+

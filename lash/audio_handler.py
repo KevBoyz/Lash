b601928f25eb259765d4dc1b5b01740c7f86ebcd
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-26 10:23:46 -0300

    new command: audio get

--- lash/audio_handler.py
+++ lash/audio_handler.py
@@ -1,6 +1,15 @@
 import click
+from moviepy.editor import VideoFileClip, AudioClip
+from lash.Exportables.fileTools import get_file
 
 
 @click.group('audio', help='Audio tools')
 def audio():
     ...
+
+
+@audio.command(help='convert a mp4 video on an mp3 file')
+@click.argument('path', metavar='<path>', type=click.Path(exists=True))
+def get(path):
+    video = VideoFileClip(path)
+    video.audio.write_audiofile(f'{path.replace(".mp4", ".mp3")}')

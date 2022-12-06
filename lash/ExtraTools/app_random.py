7b2fadef114c89f65239e5cfbd7d9388c158e863
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-12-05 22:53:39 -0300

    upgrades: undefined limit of characters

--- lash/ExtraTools/app_random.py
+++ lash/ExtraTools/app_random.py
@@ -1,31 +1,60 @@
 import click
 from random import sample, randint
+from tqdm import tqdm
+
+
+def get_size(c, n, l, s):
+    m = 0
+    if n:
+        m += 1
+    if l:
+        m += 1
+    if s:
+        m+= 1
+    return c // m
+
+def gen_random(size, n, s, l, ul, v):
+    rand_l = []
+    letters = 'qwertyuiopasdfghjklzxcvbnm'
+    symbols = '!?@#$%&*_+-'
+    if size > 1:
+        pbar = tqdm(total=size)
+    else:
+        v = False
+    for c in range(0, size):
+        if v:
+            pbar.update(1)
+        if n:
+            rand_l.append(str(randint(0, 9)))
+        if l:
+            letter = ''.join(sample(letters, 1))
+            if ul:
+                if randint(0, 1) == 1:
+                    rand_l.append(letter.upper())
+                else:
+                    rand_l.append(letter)
+            else:
+                rand_l.append(letter)
+        if s:
+            symbol = ''.join(sample(symbols, 1))
+            rand_l.append(symbol)
+    return rand_l
+
 
 @click.command(help='Generate random sequences')
-@click.option('-c', type=click.INT, default=5, show_default=True, help='Number of characters in random output')
+@click.option('-c', type=click.INT, default=5, show_default=True, help='Number of characters')
 @click.option('-n', is_flag=True, help='Enable numbers', default=True, show_default=True)
 @click.option('-l', is_flag=True, help='Enable letters', default=False, show_default=True)
 @click.option('-s', is_flag=True, help='Enable special characters', default=False, show_default=True)
 @click.option('-ul', is_flag=True, help='Enable Uppers and lowers for letters', default=False, show_default=True)
-def random(c, n, l, s, ul):
-    rand_l = []
-    if n:
-        rand_l.append('123456789')
-    if l:
-        letters = 'qwertyuiopasdfghjklzxcvbnm'
-        if ul:
-            for letter in letters:
-                if randint(0, 1) == 1:
-                    letter = letter.upper()
-                rand_l.append(letter)
-        else:
-            rand_l.append(letters)
-    if s:
-        rand_l.append('!?@#$%&*')
-    if len(rand_l) > 0:
-        try:
-            print(''.join(sample(''.join(rand_l), c)))
-        except ValueError:
-            print('Error: Number of characters exceeded the total, reduce the -c value')
-    else:
-        print('Error: Please enable something to be randomized')
+@click.option('-v', is_flag=True, help='Enable verbose mode', default=False, show_default=True)
+def random(c, n, l, s, ul, v):
+    size = get_size(c, n, l, s)
+    random_seq = gen_random(size, n, s, l, ul, v)
+    if len(random_seq) < c:
+        while len(random_seq) < c:
+            random_seq += gen_random(1, n, s, l, ul, v)
+    if len(random_seq) > c:
+        random_seq = random_seq[:c]
+
+    print(f'\n{"".join(random_seq)}\n')

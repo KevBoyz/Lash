ca22b2022d13218a1ec6bacb909e712e99ab495b
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-12-12 20:47:23 -0300

    sched_manager.py documented

--- lash/ExtraTools/sched_manager.py
+++ lash/ExtraTools/sched_manager.py
@@ -3,10 +3,16 @@ from os import system
 from time import sleep
 from datetime import datetime
 from re import search
-from typing import List
+from typing import List, Tuple
 
 
-def reg_crono(h, m, s):
+def reg_crono(h:int, m:int, s:int) -> Tuple[int, int, int]:
+    """
+    Regressive clocker.
+
+    Takes hrs,mts,scs and subtract then.
+    Return the values subtracted.
+    """
     if s > 0:
         s -= 1
     else:
@@ -43,10 +49,10 @@ def sched():
 @click.argument('s', metavar='<seconds>', type=click.INT, required=False, default=0)
 def run(command, s, m, h):
     """\b
-       Run commands repetitively at a given interval starting from
-       current moment.
-        \b
-       Example: lash sched run "help" 0 0 5 """
+       Run the command repetitively at a given interval.
+
+       \b
+       Example: run "help" 0 0 5 """
     try:
         if h <= 0 and m <= 0 and s <= 0:
             raise Exception('Error: Time delay is not defined')
@@ -71,10 +77,11 @@ def run(command, s, m, h):
 @click.argument('m', metavar='<minutes>', type=click.INT, required=False, default=0)
 @click.argument('s', metavar='<seconds>', type=click.INT, required=False, default=0)
 def wait(command, h, m, s):
-    """\b
-        Wait x time, run a task once and exit
+    """
+        Wait x time, run a command once and exit.
+
         \b
-        Example: lash sched wait "help" 0 0 10
+        Example: wait "help" 0 0 10
     """
     # assert h <= 0 and m <= 0 and s <= 0, 'Error: Time delay is not defined'
     t = h * 3600 + m * 60 + s  # Converting to seconds
@@ -92,10 +99,11 @@ def wait(command, h, m, s):
 @click.argument('command', metavar='<command>', type=click.STRING)
 def exec(time, command):
     """\b
-        Execute a task from determined moment of day
+        Execute a command from determined moment of day.
+
         \b
         The time need to have this syntax: 10:30:0
-        Command example exec 10:30:0 "help"
+        Example: exec 15:25:0 "help"
     """
     if not search('\d:\d:\d', time):
         return print('ERROR sytax incorrect, try use 10:30:0, 1:4:5 with time')
